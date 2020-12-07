72da3bf5d433c8a9beaa6eae57ed8d0976225ec0
wang960826 <849073315@qq.com>
2020-12-07 16:18:03 +0800

    whn

---
+++ src/com/idea/demo/MyDemo.java
@@ -0,0 +1,9 @@
+package com.idea.demo;
+
+public class MyDemo {
+    public static void main(String[] args){
+        String str = "sss";
+        str.length();
+        System.out.println("我的第一个Idea案例！");
+    }
+}
